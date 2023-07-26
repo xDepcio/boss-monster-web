4c50f48148972f877da7ba0d02a46170e93dfebe
xDepcio <olek.drwal@gmail.com>
2023-07-26 15:11:51 +0200

    changed gitignore

--- backend/build/src/routes/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const router = express.Router();
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
-});
-module.exports = router;
